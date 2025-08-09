1aab5abd93d5781f49f9f388ab1e712a274b34e1
Raúl Valverde <raul@fullcircle.es>
2025-08-09 11:44:07 +0200

    remove card & styles from page-wrapper

--- src/components/layout/page-wrapper.tsx
+++ src/components/layout/page-wrapper.tsx
@@ -2,7 +2,6 @@ import { Outlet } from 'react-router-dom';
 import { PageWrapperProps } from '@types';
 import { motion as Motion } from 'framer-motion';
 import { useLoading } from '@hooks';
-import { Card } from '../ui/card.tsx';
 import { Loader } from '../ui/loader.tsx';
 
 export function PageWrapper({ children, secured }: PageWrapperProps) {
@@ -16,16 +15,14 @@ export function PageWrapper({ children, secured }: PageWrapperProps) {
 			transition={{ duration: 0.1 }}
 			className={'w-full h-full'}
 		>
-			<Card className="w-full h-full min-h-[500px] min-w-[400px] overflow-scroll flex flex-col items-center justify-between gap-0 relative p-5 overflow-x-visible">
-				{(secured && true) /* required condition ex: user.authorized*/ || !secured ? (
-					<>
-						{children}
-						<Outlet />
-					</>
-				) : (
-					<Loader loading={loading.now} />
-				)}
-			</Card>
+			{(secured && true) /* required condition ex: user.authorized*/ || !secured ? (
+				<>
+					{children}
+					<Outlet />
+				</>
+			) : (
+				<Loader loading={loading.now} />
+			)}
 		</Motion.main>
 	);
 }

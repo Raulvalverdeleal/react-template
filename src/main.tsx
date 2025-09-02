e77b98e66348fda42d5a65366c492cbaa4f1a965
Raúl Valverde <raul@fullcircle.es>
2025-09-02 13:02:14 +0200

    Revert "trigger gh actios"
    This reverts commit 0a75fda7927b7e1752b6747b2da415f35e6546e1.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,4 @@ import '@styles';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router.tsx';
 
-// trigger gh actions |
-
 createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);

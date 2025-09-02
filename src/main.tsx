0a75fda7927b7e1752b6747b2da415f35e6546e1
Raúl Valverde <raul@fullcircle.es>
2025-09-02 13:01:31 +0200

    trigger gh actios

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,6 @@ import '@styles';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router.tsx';
 
+// trigger gh actions |
+
 createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
